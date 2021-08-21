6fd4e23e79840f4510ee90ff113ce556f1826bf4
mammam <correomam@gmail.com>
2021-08-21 07:11:29 +0200

    Lo elimino ya no hace falta

--- src/app/shared/global-constants.ts
+++
@@ -1,4 +0,0 @@
-export class GlobalConstants {
-  public static jsonGastos = '../../assets/data/202020210814LiqGas.json';
-  public static jsonIngresos = '../../assets/data/202020210814LiqIng.json';
-}
