86fcdc8077ae300854f481d794ba6430d3d258dd
mamjerez <correomam@gmail.com>
2022-04-10 12:17:26 +0200

    Clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,45 +15,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
+
+import { IndiceComponent } from './indice/indice.component';
 import { ComparaIngComponent } from './comparativas/compara-ing/compara-ing.component';
 import { ComparaGasComponent } from './comparativas/compara-gas/compara-gas.component';
+import { GraphIngresosComponent } from './graphs/graph-ingresos/graph-ingresos.component';
+import { GraphGastosComponent } from './graphs/graph-gastos/graph-gastos.component';
 
 import { TipoClasificacionService } from './services/tipoClasificacion.service';
 import { AvalaibleYearsService } from './services/avalaibleYears.service';
 import { HeaderAgGridComponent } from './ag-grid/header-ag-grid/header-ag-grid.component';
-import { GraphEconomicoGastoComponent } from './comparativas/graph/graph-economico-gasto/graph-economico-gasto.component';
-import { GraphProgramaComponent } from './comparativas/graph/graph-programa/graph-programa.component';
-import { GraphCapituloGastoComponent } from './comparativas/graph/graph-capitulo-gasto/graph-capitulo-gasto.component';
-import { GraphOrganicoGastoComponent } from './comparativas/graph/graph-organico-gasto/graph-organico-gasto.component';
-import { SelectCodigoComponent } from './comparativas/graph/select-codigo/select-codigo.component';
-import { GraphGrupoProgramasComponent } from './comparativas/graph/graph-grupo-programas/graph-grupo-programas.component';
-import { GraphPoliticasProgramasComponent } from './comparativas/graph/graph-politicas-programas/graph-politicas-programas.component';
-import { GraphAreasGastosComponent } from './comparativas/graph/graph-areas-gastos/graph-areas-gastos.component';
-import { IndiceComponent } from './indice/indice.component';
-import { GraphGastosEconomicaArticulosComponent } from './comparativas/graph/graph-gastos-economica-articulos/graph-gastos-economica-articulos.component';
-import { GraphGastosEconomicaConceptosComponent } from './comparativas/graph/graph-gastos-economica-conceptos/graph-gastos-economica-conceptos.component';
-import { GraphIngresosComponent } from './graphs/graph-ingresos/graph-ingresos.component';
-import { GraphGastosComponent } from './graphs/graph-gastos/graph-gastos.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
     HeaderAgGridComponent,
+    IndiceComponent,
     ComparaIngComponent,
     ComparaGasComponent,
-    GraphEconomicoGastoComponent,
-    GraphProgramaComponent,
-    GraphCapituloGastoComponent,
-    GraphOrganicoGastoComponent,
-    SelectCodigoComponent,
-    GraphGrupoProgramasComponent,
-    GraphPoliticasProgramasComponent,
-    GraphAreasGastosComponent,
-    IndiceComponent,
-    GraphGastosEconomicaArticulosComponent,
-    GraphGastosEconomicaConceptosComponent,
     GraphIngresosComponent,
     GraphGastosComponent,
   ],
