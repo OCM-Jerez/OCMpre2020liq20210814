0879000240a78c9586ce214180f2600acd4da6ab
mamjerez <correomam@gmail.com>
2022-04-18 11:18:22 +0200

    Clean code

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,22 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { GetScreenSizeService } from '../../services/get-screen-size.service';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent implements OnInit {
-  screenSize!: string
-  w!: number;
-  anio: number = new Date().getFullYear();
-
-  constructor(public getScreenSizeService: GetScreenSizeService) { }
-
-  ngOnInit() {
-    this.w = window.innerWidth;
-    this.screenSize = this.getScreenSizeService.getIsMobileResolution();
-    window.addEventListener('orientationchange', function () {
-      window.location.reload();
-    }, false);
-  }
-
+export class FooterComponent {
+  constructor() { }
 }
