c5ec499f93110293a2e4e29874ee50f988d7763f
mamjerez <correomam@gmail.com>
2022-05-01 12:29:46 +0200

    Clean code

--- src/app/graphs/graph-gastos/graph-gastos.component.ts
+++ src/app/graphs/graph-gastos/graph-gastos.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { Location } from "@angular/common";
 
 import { AgGridAngular } from 'ag-grid-angular';
@@ -7,7 +7,7 @@ import { CellRendererOCM } from '../../ag-grid/CellRendererOCM';
 
 import { accumulate } from '../../commons/util/util';
 import { DataTableGraphService } from '../../services/data-graph.service';
-import { IDataPropertyTableGraph, IDataTable, IDataGraph } from '../../commons/interfaces/dataGraph.interface';
+import { IDataGraph } from '../../commons/interfaces/dataGraph.interface';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -16,46 +16,126 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./graph-gastos.component.scss']
 })
 export class GraphGastosComponent implements OnDestroy {
-  options: AgChartOptions;
-  rowData: any;
-  data: any;
   @ViewChild('agGrid', { static: false }) agGrid: AgGridAngular;
-  private gridApi;
-  public gridColumnApi;
+  public data: any;
   public columnDefs;
   public defaultColDef;
-  public gridOptions: GridOptions;
   public localeText;
   public rowDataTable: any;
   public groupHeaderHeight = 25;
   public headerHeight = 25;
+  public options: AgChartOptions;
   private datos: any[] = [];
   private _dataTableGraph: IDataGraph;
   private _subscription: Subscription;
+
   constructor(
     private location: Location,
     private _dataTableGraphService: DataTableGraphService,
   ) {
-
     this._subscription = this._dataTableGraphService.dataSource$.subscribe((data) => {
-
       this._dataTableGraph = data;
       this._createData();
       this._createColumns()
       this._showGraph()
     });
-
-
-
   }
+
   ngOnDestroy(): void {
     if (this._subscription) {
       this._subscription.unsubscribe()
     }
   }
 
+  private async _createData() {
+    if (this._dataTableGraph.clasificationType != "aplicacion") {
+      const codigo = this._dataTableGraphService.selectedCodeRow.split(" ")[0];
+      switch (this._dataTableGraph.clasificationType) {
+        case 'gastosOrganicaOrganicos':
+          this.datos = this._dataTableGraph.data.filter(x => x.CodOrg == codigo);
+          break;
+        case 'gastosProgramaAreas':
+        case 'gastosProgramaPoliticas':
+        case 'gastosProgramaGrupos':
+        case 'gastosProgramaProgramas':
+          this.datos = this._dataTableGraph.data.filter(x => x.CodPro == codigo);
+          break;
+        case 'gastosEconomicaCapitulos':
+          this.datos = this._dataTableGraph.data.filter(x => x.CodCap == codigo);
+          break;
+        case 'gastosEconomicaArticulos':
+        case 'gastosEconomicaConceptos':
+        case 'gastosEconomicaEconomicos':
+          this.datos = this._dataTableGraph.data.filter(x => x.CodEco == codigo);
+          break;
+      }
+    } else {
+      this.datos = this._dataTableGraph.data
+    }
+
+    const yearsIniciales = accumulate('Iniciales', this.datos);
+    const yearsDefinitivas = accumulate('Definitivas', this.datos);
+    const yearsObligacionesNetas = accumulate('ObligacionesReconocidasNetas', this.datos);
+    const yearsObligacionesPendientes = accumulate('ObligacionesPendientePago', this.datos);
+
+    this.data = [];
+    for (let index = 2015; index <= 2022; index++) {
+      const value = {
+        "year": index,
+        "Definitivas": yearsDefinitivas[index],
+        "ObligacionesReconocidasNetas": yearsObligacionesNetas[index],
+        "ObligacionesPendientes": yearsObligacionesPendientes[index]
+      }
+      if (index === 2022) {
+        value.Definitivas = yearsIniciales[index]  // Se usan las iniciales ya que es el unico dato que existe.
+      }
+      this.data.push(value)
+    }
+    // console.log("Datos Tratados: ", this.data);
+    return this.data;
+  }
+
+  private _createColumns(): void {
+    this.columnDefs = [
+      {
+        headerName: 'Año',
+        field: 'year',
+        width: 70,
+      },
+      {
+        headerName: 'Previsiones definitivas',
+        field: 'Definitivas',
+        width: 180,
+        aggFunc: 'sum',
+        cellRenderer: CellRendererOCM,
+      },
+      {
+        headerName: 'ObligacionesReconocidasNetas',
+        field: 'ObligacionesReconocidasNetas',
+        width: 200,
+        aggFunc: 'sum',
+        cellRenderer: CellRendererOCM,
+      },
+      {
+        headerName: 'ObligacionesPendientes',
+        field: 'ObligacionesPendientes',
+        width: 180,
+        aggFunc: 'sum',
+        cellRenderer: CellRendererOCM,
+      }
+    ];
+
+    this.defaultColDef = {
+      sortable: true,
+      resizable: true,
+      filter: false,
+    };
+
+  }
 
   private _showGraph(): void {
+    console.log("subHeaderName", this._dataTableGraph.dataPropertyTable.subHeaderName);
+    console.log("selectedCodeRow", this._dataTableGraphService.selectedCodeRow);
     this.options = {
       autoSize: true,
       title: {
@@ -108,101 +188,6 @@ export class GraphGastosComponent implements OnDestroy {
       },
 
     }
-  }
-
-
-  private _createColumns(): void {
-
-    this.columnDefs = [
-      {
-        headerName: 'Año',
-        field: 'year',
-        width: 70,
-      },
-      {
-        headerName: 'Previsiones definitivas',
-        field: 'Definitivas',
-        width: 180,
-        aggFunc: 'sum',
-        cellRenderer: CellRendererOCM,
-      },
-      {
-        headerName: 'ObligacionesReconocidasNetas',
-        field: 'ObligacionesReconocidasNetas',
-        width: 200,
-        aggFunc: 'sum',
-        cellRenderer: CellRendererOCM,
-      },
-      {
-        headerName: 'ObligacionesPendientes',
-        field: 'ObligacionesPendientes',
-        width: 180,
-        aggFunc: 'sum',
-        cellRenderer: CellRendererOCM,
-      }
-    ];
-
-    this.defaultColDef = {
-      sortable: true,
-      resizable: true,
-      filter: false,
-    };
-
-  }
-
-  async onGridReady(params) {
-    // this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-  }
-
-  private async _createData() {
-    if (this._dataTableGraph.clasificationType != "aplicacion") {
-      const codigo = this._dataTableGraphService.selectedCodeRow.split(" ")[0];
-      switch (this._dataTableGraph.clasificationType) {
-        case 'gastosOrganicaOrganicos':
-          this.datos = this._dataTableGraph.data.filter(x => x.CodOrg == codigo);
-          break;
-        case 'gastosProgramaAreas':
-        case 'gastosProgramaPoliticas':
-        case 'gastosProgramaGrupos':
-        case 'gastosProgramaProgramas':
-          this.datos = this._dataTableGraph.data.filter(x => x.CodPro == codigo);
-          break;
-        case 'gastosEconomicaCapitulos':
-          this.datos = this._dataTableGraph.data.filter(x => x.CodCap == codigo);
-          break;
-        case 'gastosEconomicaArticulos':
-        case 'gastosEconomicaConceptos':
-        case 'gastosEconomicaEconomicos':
-          this.datos = this._dataTableGraph.data.filter(x => x.CodEco == codigo);
-          break;
-      }
-    } else {
-      this.datos = this._dataTableGraph.data
-    }
-
-
-
-    const yearsIniciales = accumulate('Iniciales', this.datos);
-    const yearsDefinitivas = accumulate('Definitivas', this.datos);
-    const yearsObligacionesNetas = accumulate('ObligacionesReconocidasNetas', this.datos);
-    const yearsObligacionesPendientes = accumulate('ObligacionesPendientePago', this.datos);
-
-    this.data = [];
-    for (let index = 2015; index <= 2022; index++) {
-      const value = {
-        "year": index,
-        "Definitivas": yearsDefinitivas[index],
-        "ObligacionesReconocidasNetas": yearsObligacionesNetas[index],
-        "ObligacionesPendientes": yearsObligacionesPendientes[index]
-      }
-      if (index === 2022) {
-        value.Definitivas = yearsIniciales[index]  // Se usan las iniciales ya que es el unico dato que existe.
-      }
-      this.data.push(value)
-    }
-    // console.log("Datos Tratados: ", this.data);
-    return this.data;
   }
 
   volver() {
