73824495d9379bfad5d26c87e625c7b67e057d4b
mamjerez <correomam@gmail.com>
2022-05-20 16:37:25 +0200

    Select only 2021

--- src/app/commons/components/checkbox/checkbox.component.ts
+++ src/app/commons/components/checkbox/checkbox.component.ts
@@ -17,30 +17,30 @@ export class CheckboxComponent implements OnInit {
     this.years = [
       {
         year: 2015,
-        checked: true,
+        checked: false,
       },
       {
         year: 2016,
-        checked: true,
+        checked: false,
       }, {
         year: 2017,
-        checked: true,
+        checked: false,
       }, {
         year: 2018,
-        checked: true,
+        checked: false,
       },
       {
         year: 2019,
-        checked: true,
+        checked: false,
       }, {
         year: 2020,
-        checked: true,
+        checked: false,
       }, {
         year: 2021,
         checked: true,
       }, {
         year: 2022,
-        checked: true,
+        checked: false,
       },
     ]
     this.getSelectedItem();
